840e5c350702c8843b81a101d2506b25cce07d7e
Grinchenko Sergei <butters.blaga@gmail.com>
2022-11-29 22:38:13 +0900

    feat: add Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,17 @@ import './Footer.css'
 const Footer = () => {
   return (
     <footer className="footer">
-      Footer
+      <div className="footer__container">
+        <h3 className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</h3>
+        <div className="footer__content">
+          <p className="footer__copyright">&copy; Сергей Гринченко. 2022</p>
+          <ul className="footer__list">
+            <li className="footer__list-item"><a href="https://practicum.yandex.ru" className="footer__link" target="_blank" rel="noopener noreferrer">Яндекс.Практикум</a></li>
+            <li className="footer__list-item"><a href="https://github.com/Butterzzz" className="footer__link" target="_blank" rel="noopener noreferrer">GitHub</a></li>
+            <li className="footer__list-item"><a href="https://t.me/Butterzzz" className="footer__link" target="_blank" rel="noopener noreferrer">Telegram</a></li>
+          </ul>
+        </div>
+      </div>
     </footer>
   )
 }
