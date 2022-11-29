94bb33994a0e1054cdab4fede66725170b0e7e61
Grinchenko Sergei <butters.blaga@gmail.com>
2022-11-29 22:03:08 +0900

    feat: add Header component (for landing)

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import Logo from '../Logo/Logo'
 import './Header.css'
 
 const Header = () => {
   return (
     <header className="header">
-      Header
-    </header>
+      <div className="header__container">
+        <Logo />
+        <nav className="header__menu">
+          <NavLink to="/signup" className="header__signup">Регистрация</NavLink>
+          <NavLink to="/signin" className="header__signin">Войти</NavLink>
+        </nav>
+      </div >
+    </header >
   )
 }
 
