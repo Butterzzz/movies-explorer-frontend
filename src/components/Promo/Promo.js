1d07198387de991b9381e37e84eead62bfb21dd8
Grinchenko Sergei <butters.blaga@gmail.com>
2022-12-01 00:57:26 +0900

    feat: add Promo component

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import promoLogo from '../../images/promo-logo.svg'
 import './Promo.css'
 
 const Promo = () => {
   return (
     <section className="promo">
-      Promo
+      <div className="promo__container">
+        <div className="promo__content">
+          <h1 className="promo__title">Учебный проект студента факультета Веб&#8209;разработки.</h1>
+          <p className="promo__subtitle">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
+          <a href="#about" className="promo__link">Узнать больше</a>
+        </div>
+        <img className="promo__image" src={promoLogo} alt="Планета Земля из слов WEB" />
+      </div>
     </section>
   )
 }
