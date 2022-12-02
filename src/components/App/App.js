a1483144fb82f38ee4d2472693fd83b3850d7701
Grinchenko Sergei <butters.blaga@gmail.com>
2022-12-03 00:28:42 +0900

    feat: add routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,50 @@
 import React from 'react'
+import { Switch, Route } from 'react-router-dom';
 import './App.css'
 import Header from '../Header/Header'
 import Main from '../Main/Main'
+import Register from '../Register/Register'
+import Login from '../Login/Login'
 import Footer from '../Footer/Footer'
 
 const App = () => {
   return (
     <div className="page">
-      <Header />
-      <Main />
-      <Footer />
+      <Switch>
+
+        <Route exact path="/">
+          <Header />
+          <Main />
+          <Footer />
+        </Route>
+
+        <Route path="/movies">
+          Movies will be here
+        </Route>
+
+        <Route path="/saved-movies">
+          Saved movies will be here
+        </Route>
+
+        <Route path="/profile">
+        <Header />
+          Profile will be here
+        </Route>
+
+        <Route path="/signup">
+          <Register />
+        </Route>
+
+        <Route path="/signin">
+          <Login />
+        </Route>
+
+        <Route path="*">
+          PageNotFound will be here
+        </Route>
+
+      </Switch>
+
     </div>
   )
 }
