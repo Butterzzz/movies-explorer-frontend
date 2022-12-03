d9397e622d0411214bb8223205da7e7ee2a73a25
Grinchenko Sergei <butters.blaga@gmail.com>
2022-12-04 04:10:37 +0900

    feat: add Movies component

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import SearchForm from '../SearchForm/SearchForm'
+// import Preloader from '../Preloader/Preloader'
+import MoviesCardList from '../MoviesCardList/MoviesCardList'
 import './Movies.css'
 
-const Movies = () => {
+const Movies = ({ movies }) => {
   return (
-    <div>Movies</div>
+    <main className="content">
+      <SearchForm />
+      {/* <Preloader /> */}
+      {movies && <MoviesCardList
+        moviesList={movies}
+      />
+      }
+      <div className="movies__more-button-container">
+        <button className="movies__more-button" type="button">Ещё</button>
+      </div>
+    </main>
   )
 }
 
