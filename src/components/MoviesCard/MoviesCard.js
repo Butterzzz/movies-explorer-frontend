16ed76881955460782fd9fcf3ad6889f0309590c
Grinchenko Sergei <butters.blaga@gmail.com>
2022-12-04 03:13:49 +0900

    feat: add MoviesCard component

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import './MoviesCard.css'
 
-const MoviesCard = () => {
+const MoviesCard = ({ movie }) => {
   return (
-    <div>MoviesCard</div>
+    <li className="movies__list-item">
+      <article className="card">
+        <img className="card__image" src={movie.image} alt={movie.nameRU} />
+        <div className="card__description">
+          <div className="card__wrapper">
+            <h2 className="card__title">{movie.nameRU}</h2>
+            <button className="card__button" type="button" aria-label="Cохранить в коллекцию" />
+          </div>
+          <p className="card__duration">{movie.duration}</p>
+        </div>
+      </article>
+    </li>
   )
 }
 
