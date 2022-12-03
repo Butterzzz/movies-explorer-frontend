e11d0aac645c886ade71dd2eef32e79bca6142a5
Grinchenko Sergei <butters.blaga@gmail.com>
2022-12-03 23:13:35 +0900

    feat: add SearchForm component

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import RoundedSwitch from '../RoundedSwitch/RoundedSwitch'
 import './SearchForm.css'
 
 const SearchForm = () => {
   return (
-    <div>SearchForm</div>
+    <section className="search">
+      <div className="search__container">
+
+        <form className="search-form">
+          <span className="search-form__icon"></span>
+          <input className="search-form__input" type="text" placeholder="Фильм" minLength="2" required />
+          <button className="search-form__button" type="submit" aria-label="Поиск"></button>
+        </form>
+
+        <RoundedSwitch />
+
+      </div>
+    </section>
   )
 }
 
