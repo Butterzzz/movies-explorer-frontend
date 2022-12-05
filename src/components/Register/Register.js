3b6fdafb89413918b578267e0981bd7a48aa6c2e
Grinchenko Sergei <butters.blaga@gmail.com>
2022-12-06 00:19:29 +0900

    feat: add Register component

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,9 +1,53 @@
 import React from 'react'
+import Form from '../Form/Form'
 import './Register.css'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <section className="register">
+      <Form
+        title="Добро пожаловать!"
+        name="register-form"
+        submitText="Зарегистрироваться"
+        isRegister={true}
+      >
+        <label className="form__field" htmlFor="register-name">Имя</label>
+        <input
+          className="form__input"
+          id="register-name"
+          type="text"
+          name="name"
+          placeholder="Имя"
+          required
+        />
+
+        <span className="form__error">Ошибка</span>
+
+        <label className="form__field" htmlFor="register-email">E-mail</label>
+        <input
+          className="form__input"
+          id="register-email"
+          type="email"
+          name="email"
+          placeholder="E-mail"
+          required
+        />
+
+        <span className="form__error">Ошибка</span>
+
+        <label className="form__field" htmlFor="register-password">Пароль</label>
+        <input
+          className="form__input"
+          id="register-password"
+          type="password"
+          name="password"
+          placeholder="Пароль"
+          required
+        />
+
+        <span className="form__error">Ошибка</span>
+      </Form>
+    </section>
   )
 }
 
