8d2e6415e033079aad924c65b33ca3a63981e3f6
Grinchenko Sergei <butters.blaga@gmail.com>
2022-12-06 00:22:52 +0900

    feat: add Login component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,41 @@
 import React from 'react'
+import Form from '../Form/Form'
 import './Login.css'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <section className="login">
+      <Form
+        title="Рады видеть!"
+        name="login-form"
+        submitText="Войти"
+        isLogin={true}
+      >
+        <label className="form__field" htmlFor="login-email">E-mail</label>
+        <input
+          className="form__input"
+          id="login-email"
+          type="email"
+          name="email"
+          placeholder="E-mail"
+          required
+        />
+
+        <span className="form__error">Ошибка</span>
+
+        <label className="form__field" htmlFor="login-password">Пароль</label>
+        <input
+          className="form__input"
+          id="login-password"
+          type="password"
+          name="password"
+          placeholder="Пароль"
+          required
+        />
+
+        <span className="form__error">Ошибка</span>
+      </Form>
+    </section>
   )
 }
 
