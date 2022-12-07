75c5b14fa590e49d97285e1a03ceac30b2d1b60e
Grinchenko Sergei <butters.blaga@gmail.com>
2022-12-08 03:48:41 +0900

    feat: add header color change

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useLocation } from 'react-router-dom'
 import Logo from '../Logo/Logo'
 import Navigation from '../Navigation/Navigation'
 import './Header.css'
 
 const Header = ({ isAuth }) => {
+  const location = useLocation();
+
   return (
-    <header className="header">
+    <header className={`header ${location.pathname ==='/' && 'header_type_colored'}`}>
       <div className="header__container">
         <Logo />
         {isAuth ? <Navigation /> : <nav className="header__menu">
