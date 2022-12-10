b9bb606be1e351ed4f8a521cd9513a93fbb177b3
Grinchenko Sergei <butters.blaga@gmail.com>
2022-12-11 03:54:35 +0900

    refactor: PopupMenu component

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,26 +1,80 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
+import PopupMenu from '../PopupMenu/PopupMenu'
 import './Navigation.css'
 
 const Navigation = ({ isLoggedIn }) => {
+  const [isMenuPopupOpen, setIsMenuPopupOpen] = useState(false);
+
+  function handleOpenMenuClick() {
+    setIsMenuPopupOpen(true);
+  }
+
+  function closeAllPopups() {
+    setIsMenuPopupOpen(false);
+  }
+
   return (
     <>
       {isLoggedIn ? (
         <>
           <nav className="menu" >
             <ul className="menu__list menu__list-auth">
-              <li className="menu__list-item"><NavLink to='/movies' className="menu__link link" activeClassName="menu__link_active">Фильмы</NavLink></li>
-              <li className="menu__list-item"><NavLink to='/saved-movies' className="menu__link link" activeClassName="menu__link_active">Сохранённые фильмы</NavLink></li>
+
+              <li className="menu__list-item">
+                <NavLink
+                  to='/movies'
+                  className="menu__link link"
+                  activeClassName="menu__link_active">
+                  Фильмы
+                </NavLink>
+              </li>
+
+              <li className="menu__list-item">
+                <NavLink
+                  to='/saved-movies'
+                  className="menu__link link"
+                  activeClassName="menu__link_active">
+                  Сохранённые фильмы
+                </NavLink>
+              </li>
+
             </ul>
-            <NavLink to="/profile" className="menu__profile button">Аккаунт</NavLink>
-            <button className="menu__button button" type="button" />
+
+            <NavLink
+              to="/profile"
+              className="menu__profile button">
+              Аккаунт
+            </NavLink>
+
+            <button className="menu__button button" type="button" onClick={handleOpenMenuClick} />
+
+            {isLoggedIn && <PopupMenu
+              isOpen={isMenuPopupOpen}
+              onClose={closeAllPopups}
+            />}
           </nav >
         </>
       ) : (
         <nav className="menu">
           <ul className="menu__list menu__list_not-auth">
-            <li className="menu__list-item"><NavLink to="/signup" className="menu__link menu__link_type_signup link">Регистрация</NavLink></li>
-            <li className="menu__list-item"><NavLink to="/signin" className="menu__link menu__link_type_signin button">Войти</NavLink></li>
+
+            <li className="menu__list-item">
+              <NavLink
+                to="/signup"
+                className="menu__link menu__link_type_signup link">
+                Регистрация
+              </NavLink>
+            </li>
+
+            <li className="menu__list-item">
+              <NavLink
+                to="/signin"
+                className="menu__link menu__link_type_signin button">
+                Войти
+              </NavLink>
+            </li>
+
           </ul>
         </nav>)}
     </>
--- src/components/PopupMenu/PopupMenu.js
+++ src/components/PopupMenu/PopupMenu.js
@@ -1,25 +1,65 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { NavLink } from 'react-router-dom'
 import './PopupMenu.css'
 
 const PopupMenu = ({ isOpen, onClose }) => {
+  useEffect(() => {
+    const closeByEsc = (evt) => {
+      if (evt.key === 'Escape') {
+        onClose();
+      }
+    }
+    document.addEventListener('keydown', closeByEsc)
+    return () => document.removeEventListener('keydown', closeByEsc)
+  }, [onClose])
+
   return (
-    <div className={`popup ${isOpen && "popup_opened"}`}>
+    <div className={`popup ${isOpen && "popup_opened"}`} onClick={onClose}>
       <div className="popup__container">
 
         <nav className="popup__menu" >
           <ul className="popup__menu-list">
+
             <li className="popup__menu-list-item">
-              <NavLink to='/' className="popup__menu-link link" activeClassName="popup__menu-link_active">Главная</NavLink>
+              <NavLink
+                to='/'
+                className="popup__menu-link link"
+                onClick={onClose}
+              >
+                Главная
+              </NavLink>
             </li>
+
             <li className="popup__menu-list-item">
-              <NavLink to='/movies' className="popup__menu-link link" activeClassName="popup__menu-link_active">Фильмы</NavLink>
+              <NavLink
+                to='/movies'
+                className="popup__menu-link link"
+                activeClassName="popup__menu-link_active"
+                onClick={onClose}
+              >
+                Фильмы
+              </NavLink>
             </li>
+
             <li className="popup__menu-list-item">
-              <NavLink to='/saved-movies' className="popup__menu-link link" activeClassName="popup__menu-link_active">Сохранённые фильмы</NavLink>
+              <NavLink
+                to='/saved-movies'
+                className="popup__menu-link link"
+                activeClassName="popup__menu-link_active"
+                onClick={onClose}
+              >
+                Сохранённые фильмы
+              </NavLink>
             </li>
           </ul>
-          <NavLink to="/profile" className="popup__menu-profile button">Аккаунт</NavLink>
+
+          <NavLink
+            to="/profile"
+            className="popup__menu-profile button"
+            onClick={onClose}
+          >
+            Аккаунт
+          </NavLink>
         </nav >
 
       </div>
@@ -28,7 +68,9 @@ const PopupMenu = ({ isOpen, onClose }) => {
         className="popup__button popup__button_action_close button"
         type="button"
         aria-label="Закрыть меню"
-        onClick={onClose} />
+        onClick={onClose}
+      />
+
     </div>
   )
 }
