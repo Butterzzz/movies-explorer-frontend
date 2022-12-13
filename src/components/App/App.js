8a3d1b83ec8786fc32b560d59a0bc1a10ef526f3
Grinchenko Sergei <butters.blaga@gmail.com>
2022-12-13 18:12:14 +0900

    feat: add user context

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { CurrentUserContext } from '../../contexts/CurrentUserContext'
 import { Switch, Route } from 'react-router-dom'
 import './App.css'
 import Header from '../Header/Header'
@@ -13,61 +14,63 @@ import Footer from '../Footer/Footer'
 import moviesList from '../../data/movies'
 
 const App = () => {
+
+  const [currentUser, setCurrentUser] = useState({});
   const [movies, setMovies] = useState(null);
   useEffect(() => {
     setMovies(moviesList);
   }, [])
 
   return (
-    <div className="page">
-      <Switch>
+    <CurrentUserContext.Provider value={currentUser}>
+      <div className="page">
+        <Switch>
+          <Route exact path="/">
+            <Header />
+            <Main />
+            <Footer />
+          </Route>
 
-        <Route exact path="/">
-          <Header />
-          <Main />
-          <Footer />
-        </Route>
+          <Route path="/movies">
+            <Header
+              isLoggedIn={true}
+            />
+            <Movies
+              movies={movies} />
+            <Footer />
+          </Route>
 
-        <Route path="/movies">
-          <Header
-            isLoggedIn={true}
-          />
-          <Movies
-            movies={movies} />
-          <Footer />
-        </Route>
+          <Route path="/saved-movies">
+            <Header
+              isLoggedIn={true}
+            />
+            <SavedMovies
+              movies={movies} />
+            <Footer />
+          </Route>
 
-        <Route path="/saved-movies">
-          <Header
-            isLoggedIn={true}
-          />
-          <SavedMovies
-            movies={movies} />
-          <Footer />
-        </Route>
+          <Route path="/profile">
+            <Header
+              isLoggedIn={true}
+            />
+            <Profile />
+          </Route>
 
-        <Route path="/profile">
-          <Header
-            isLoggedIn={true}
-          />
-          <Profile />
-        </Route>
+          <Route path="/signup">
+            <Register />
+          </Route>
 
-        <Route path="/signup">
-          <Register />
-        </Route>
+          <Route path="/signin">
+            <Login />
+          </Route>
 
-        <Route path="/signin">
-          <Login />
-        </Route>
+          <Route path="*">
+            <PageNotFound />
+          </Route>
 
-        <Route path="*">
-          <PageNotFound />
-        </Route>
-
-      </Switch>
-
-    </div>
+        </Switch>
+      </div>
+    </CurrentUserContext.Provider>
   )
 }
 
