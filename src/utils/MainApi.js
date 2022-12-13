db82272193afa8327fdabb5d32b1018a161d98d1
Grinchenko Sergei <butters.blaga@gmail.com>
2022-12-14 02:19:35 +0900

    feat: add MainApi

---
+++ src/utils/MainApi.js
@@ -0,0 +1,126 @@
+export const BASE_URL = 'api.movies-explorer.gss.nomoredomains.club';
+
+function checkServerResponse(res) {
+    if (res.ok) {
+        return res.json();
+    }
+    return Promise.reject(`Ошибка: ${res.status}`);
+}
+
+// Регистрация
+export const register = (name, email, password) => {
+    return fetch(`${BASE_URL}/signup`, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ name, email, password })
+    })
+        .then(checkServerResponse);
+};
+
+// Авторизация (логин)
+export const authorize = (email, password) => {
+    return fetch(`${BASE_URL}/signin`, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('jwt')}`
+        },
+        body: JSON.stringify({ email, password })
+    })
+        .then(checkServerResponse);
+};
+
+// Получаем информацию о пользователе
+export const getUserInfo = (token) => {
+    return fetch(`${BASE_URL}/users/me`, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('jwt')}`
+        }
+    })
+        .then(checkServerResponse);
+}
+
+// Обновляем информацию о пользователе
+export const updateUserInfo = (name, email) => {
+    return fetch(`${BASE_URL}/users/me`, {
+        method: 'PATCH',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('jwt')}`
+        },
+        body: JSON.stringify({ name, email })
+    })
+        .then(checkServerResponse);
+}
+
+// Получаем все сохранённые текущим  пользователем фильмы
+export const getSavedMovies = () => {
+    return fetch(`${BASE_URL}/movies`, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('jwt')}`
+        }
+    })
+        .then(checkServerResponse);
+}
+
+// Создаём (сохраняем) фильм
+export const createMovie = ({
+    country,
+    director,
+    duration,
+    year,
+    description,
+    image,
+    trailerLink,
+    nameRU,
+    nameEN,
+    thumbnail,
+    movieId,
+}) => {
+    return fetch(`${BASE_URL}/movies`, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('jwt')}`
+        },
+        body: JSON.stringify({
+            country,
+            director,
+            duration,
+            year,
+            description,
+            image,
+            trailerLink,
+            nameRU,
+            nameEN,
+            thumbnail,
+            movieId,
+        })
+    })
+        .then(checkServerResponse);
+};
+
+// Удаляем сохранённый фильм по id
+export const deleteMovie = (movieId) => {
+    return fetch(`${BASE_URL}/movies/${movieId}`, {
+        method: 'DELETE',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('jwt')}`
+        }
+    })
+        .then(checkServerResponse);
+}
