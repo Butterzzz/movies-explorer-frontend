be8aa11e9cd1d339be0fbb45983eee9d8d329bd2
Grinchenko Sergei <butters.blaga@gmail.com>
2022-12-14 04:15:38 +0900

    feat: add ProtectedRoute component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { CurrentUserContext } from '../../contexts/CurrentUserContext'
 import { Switch, Route } from 'react-router-dom'
 import './App.css'
+import ProtectedRoute from '../ProtectedRoute/ProtectedRoute'
 import Header from '../Header/Header'
 import Main from '../Main/Main'
 import Movies from '../Movies/Movies'
@@ -12,10 +13,14 @@ import Login from '../Login/Login'
 import PageNotFound from '../PageNotFound/PageNotFound'
 import Footer from '../Footer/Footer'
 import moviesList from '../../data/movies'
+// import * as MainApi from '../../utils/MainApi'
+// import * as MoviesApi from '../../utils/MoviesApi'
 
 const App = () => {
 
   const [currentUser, setCurrentUser] = useState({});
+  const [isLoggedIn, setisIsLoggedIn] = useState(false);
+
   const [movies, setMovies] = useState(null);
   useEffect(() => {
     setMovies(moviesList);
@@ -31,30 +36,22 @@ const App = () => {
             <Footer />
           </Route>
 
-          <Route path="/movies">
-            <Header
-              isLoggedIn={true}
-            />
-            <Movies
-              movies={movies} />
+          <ProtectedRoute path="/movies" isLoggedIn={isLoggedIn}>
+            <Header isLoggedIn={isLoggedIn} />
+            <Movies movies={movies} />
             <Footer />
-          </Route>
+          </ProtectedRoute>
 
-          <Route path="/saved-movies">
-            <Header
-              isLoggedIn={true}
-            />
-            <SavedMovies
-              movies={movies} />
+          <ProtectedRoute path="/saved-movies" isLoggedIn={isLoggedIn}>
+            <Header isLoggedIn={isLoggedIn} />
+            <SavedMovies movies={movies} />
             <Footer />
-          </Route>
+          </ProtectedRoute>
 
-          <Route path="/profile">
-            <Header
-              isLoggedIn={true}
-            />
+          <ProtectedRoute path="/profile" isLoggedIn={isLoggedIn}>
+            <Header isLoggedIn={isLoggedIn} />
             <Profile />
-          </Route>
+          </ProtectedRoute>
 
           <Route path="/signup">
             <Register />
